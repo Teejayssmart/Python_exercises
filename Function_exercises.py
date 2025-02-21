496fab14ff47d1d40d5c5ba3c34a1e6e500722a0
Teejay Adedosu <teejay.adedosu@toshh.online>
2025-02-21 09:58:16 +0000

    Daily exercises on function

---
+++ Function_exercises.py
@@ -0,0 +1,11 @@
+# Sum of Digits
+# Write a function sum_of_digits(n)
+# that takes an integer n and returns the sum of its digits.
+
+def sum_of_digits(n):
+    sum = n + n
+    return sum
+
+
+result = sum_of_digits(19)
+print(result)
